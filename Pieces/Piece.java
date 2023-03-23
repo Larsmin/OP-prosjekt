d0422ff61b7727789e45376fb00fe67a10eea54f
larsmind <larsmind@stud.ntnu.no>
2023-03-23 16:42:10 +0100

    litt piece kode

--- Pieces/Piece.java
+++ Pieces/Piece.java
@@ -1,3 +1,14 @@
 package Pieces;
+
 public abstract class Piece {
+    private boolean captured;
+    private boolean white;
+    
+    Piece(boolean white) {
+        this.white = white;
+    }
+
+    public boolean isWhite() {
+        return this.white;
+    }
 }
