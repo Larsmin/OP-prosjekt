c7baf6cbf385f56f3ac93d08e7d0ee679844224b
Lars Mindrebø <larsmind@stud.ntnu.no>
2023-03-24 19:23:25 +0100

    litt square logikk

--- Square.java
+++ Square.java
@@ -1,3 +1,38 @@
-public class Square{
+public class Square {
+    private Piece piece;
+    private int x;
+    private int y;
+
+    Square(Piece piece, int x, int y) {
+        this.piece = piece;
+        this.x = x;
+        this.y = y; 
+    }
+    // Returns the piece on a square, if the square does not have a piece this.piece will be None.
+    public Piece getPiece() {
+        return this.piece;
+    }
+    // Puts a new piece on the square.
+    public void setPiece(Piece p) {
+        this.piece = p;
+
+    }
+    // Returns the x-coordinate of the square.
+    public int getX() {
+        return this.x;
+    }
+    // Sets the x coordinate of the square, (When will this be used?)
+    public void setX(int x) {
+        this.x = x;
+    }
+    // Returns the y-coordinate of the square.
+    public int getY() {
+        return this.y;
+    }
+    public int setY(int y) {
+        this.y = y;
+    }
+
+
 
 }
